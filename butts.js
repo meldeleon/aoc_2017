48521b8a982521f4b7aeb7321da17c2893f8a2e9
meldeleon <melissadeleon@gmail.com>
2022-10-03 13:02:04 -0700

    finishing day 9 pt 2

--- butts.js
+++ butts.js
@@ -1,15 +1,1 @@
-let butts = {
-  ass: "phat",
-  booty: "thicc",
-}
-
-function test(prop) {
-  if (butts.ass) {
-    console.log("truthy")
-  } else {
-    console.log("falsy")
-  }
-}
-
-test(butts.ass)
-test(butts.butthole)
+let butts = ["butts", "booty", "!"]
--- day09_pt2.js
+++ day09_pt2.js
@@ -1,13 +1,20 @@
 const input = require("fs").readFileSync("day09_input.txt").toString()
 
-let test = input
+let test = []
+for (let i = 0; i < input.length; i++) {
+  test.push(input[i])
+}
 let state = {
   nestingLevel: 0,
   garbage: false,
   total: 0,
 }
 
+//test is primary iterative source
+//target is copy of test, that we will mutate
+
 // parse basic nested {}
+let target = [...test]
 for (let i = 0; i < test.length; i++) {
   //console.log(`char: ${test[i]}`)
   if (state.garbage) {
@@ -19,6 +26,8 @@ for (let i = 0; i < test.length; i++) {
       case "!":
         i++
         break
+      default:
+        target.splice(i, 1, "*")
     }
   } else {
     switch (test[i]) {
@@ -37,4 +46,14 @@ for (let i = 0; i < test.length; i++) {
   //console.log({ state })
 }
 
-console.log(`The answer is ${state.total}`)
+//now that we have marked target with placeholder removal "!" we will remove it from target
+let count = 0
+target.forEach((x) => {
+  if (x === "*") {
+    count++
+  } else {
+  }
+})
+
+console.log(target)
+console.log(`The answer is ${count}`)
